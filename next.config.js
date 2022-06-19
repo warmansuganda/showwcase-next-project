49b8e6fa0c3596084498132af3ee0bbf77e49f8d
Warman Suganda <warman.suganda@gmail.com>
2022-06-19 11:46:23 +0700

    feat: implement PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
   reactStrictMode: true,
-};
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ function Home() {
         </div>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
+      <footer className="mt-10 flex h-24 w-full items-center justify-center border-t">
         <a
           className="flex items-center justify-center gap-2"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
