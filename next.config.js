9b6e5c0f44f41b9187dce3bc27286afe7d74497f
Warman Suganda <warman.suganda@gmail.com>
2022-06-21 23:12:40 +0700

    feat: add runtimeCaching

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
+    runtimeCaching,
   },
   reactStrictMode: true,
 });
